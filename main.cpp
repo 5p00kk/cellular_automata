06f0c546215d56748ec664a4db7fcdcbedac6712
5p00kk <szymon.kowalewski@gmail.com>
2022-04-06 12:28:00 +0200

    fix the rule 30

--- main.cpp
+++ main.cpp
@@ -43,16 +43,16 @@ int main()
     */
     /*
     Rule 30
-    0 -> 0
-    1 -> 1
-    2 -> 1
-    3 -> 1
-    4 -> 1
-    5 -> 0
-    6 -> 0
-    7 -> 0
+    0 -> 1
+    1 -> 0
+    2 -> 0
+    3 -> 0
+    4 -> 0
+    5 -> 1
+    6 -> 1
+    7 -> 1
     */
-    std::array<uint8_t, 8> rule = {0,1,1,1,1,0,0,0};
+    std::array<uint8_t, 8> rule = {1,0,0,0,0,1,1,1};
     print_rule(rule);
 
     for(int row = 0; row < world.rows-10; row++)
