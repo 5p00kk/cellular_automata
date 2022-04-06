6b16ff7074f3d8f94bebae0f508cfb1411a437a5
5p00kk <szymon.kowalewski@gmail.com>
2022-04-06 10:39:04 +0200

    Create world matrix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <opencv2/core/mat.hpp>
 
 int main()
 {
-    printf("Starting cellular automata\n");
+    /* Create the world */
+    cv::Mat world = cv::Mat::zeros(100, 100, CV_8UC1);
+    
+    /* Printouts */
+    printf("\nStarting cellular automata\n");
+    printf("World size: %d, %d\n", world.cols, world.rows);
+    printf("\n");
 }
