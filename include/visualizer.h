754edeced5f54051b527ef13ab865072f18c1a86
5p00kk <szymon.kowalewski@gmail.com>
2022-04-08 14:46:03 +0200

    fix type/missing semicols

--- include/visualizer.h
+++ include/visualizer.h
@@ -10,13 +10,13 @@ struct s_color
     uint8_t r = 0;
     uint8_t g = 0;
     uint8_t b = 0;
-}
+};
 
 class c_visualizer
 {
     public:
         void visualize(const cv::Mat &world, cv::Mat &visuals) const;
-        void set_mode(e_viso mode);
+        void set_mode(e_visu mode);
         void add_color_mapping(uint8_t val, s_color color);
         void add_bw_mapping(uint8_t val, uint8_t bw);
 
@@ -24,4 +24,4 @@ class c_visualizer
         e_visu m_mode = e_visu::bw;
         std::map<uint8_t, uint8_t> m_bw_map{};
         std::map<uint8_t, s_color> m_color_map{};
-}
+};
