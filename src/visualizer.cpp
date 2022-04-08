81ddebd3677aea97399e91f6b4a05629dd29d9bc
5p00kk <szymon.kowalewski@gmail.com>
2022-04-08 15:05:35 +0200

    add bw visu implementation

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -14,7 +14,21 @@ void c_visualizer::visualize(const cv::Mat &world, cv::Mat &visuals) const
 
 void c_visualizer::visualize_bw(const cv::Mat &world, cv::Mat &visuals) const
 {
-    /* placeholder */
+    /* Allocate correct mat if needed */
+    if(visuals.rows != world.rows ||
+       visuals.cols != world.cols ||
+       visuals.type() != CV_8UC1)
+    {
+        visuals.create(world.rows, world.cols, CV_8UC1);
+    }
+
+    for(int row = 0; row < world.rows; row++)
+    {
+        for(int col = 0; col < world.cols; col++)
+        {
+            visuals.at<uint8_t>(row,col) = m_bw_map[world.at<uint8_t>(row,col)];
+        }
+    }
 }
 
 void c_visualizer::visualize_col(const cv::Mat &world, cv::Mat &visuals) const
