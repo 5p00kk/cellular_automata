c5cc625fb1f826da9eb11e376e81f7e31d5afaf3
5p00kk <szymon.kowalewski@gmail.com>
2022-04-08 15:05:12 +0200

    use array instead of map

--- include/visualizer.h
+++ include/visualizer.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <opencv2/core/mat.hpp>
-#include <map>
+#include <array>
 
 enum class e_visu {bw, col};
 
@@ -23,6 +23,6 @@ class c_visualizer
         void visualize_bw(const cv::Mat &world, cv::Mat &visuals) const;
         void visualize_col(const cv::Mat &world, cv::Mat &visuals) const;
         e_visu m_mode = e_visu::bw;
-        std::map<uint8_t, uint8_t> m_bw_map{};
-        std::map<uint8_t, s_color> m_color_map{};
+        std::array<uint8_t, 256> m_bw_map;
+        std::array<s_color, 256> m_color_map;
 };
--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -29,10 +29,10 @@ void c_visualizer::set_mode(e_visu mode)
 
 void c_visualizer::add_color_mapping(uint8_t val, s_color color)
 {
-    m_color_map.insert(std::pair<uint8_t, s_color>(val, color));
+    m_color_map[val] = color;
 }
 
 void c_visualizer::add_bw_mapping(uint8_t val, uint8_t bw)
 {
-    m_bw_map.insert(std::pair<uint8_t, uint8_t>(val, bw));
+    m_bw_map[val] = bw;
 }
